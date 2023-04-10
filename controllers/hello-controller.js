9a3320bc2eb92bc00e9386fc2e72f682c1b0709d
Charlotte <charlottemay.234@gmail.com>
2023-04-10 00:04:01 -0700

    debug change

--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -3,7 +3,7 @@ app.get('/hello', (req, res) => {
     res.send('Life is good!')
  });
  app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')
+    res.send('Welcome to Full Stack Development A9!')
  });
 }
 export default HelloController;
