031e8a787f8f1d9d4b83e24bc6170bbc678bd86b
Charlotte <charlottemay.234@gmail.com>
2023-04-10 00:02:40 -0700

    debug changes

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter';
 console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('open', () => {
+    if (mongoose.connection.readyState === 1) {
+      console.log('Connected to MongoDB database');
+    } else {
+      console.log('Error connecting to MongoDB database');
+    }
+});
 const app = express()
 app.use(cors()) // configure cors right after instantiating express
 app.use(express.json())
